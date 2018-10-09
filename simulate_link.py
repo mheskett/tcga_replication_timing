6527bc66fdafef5fcceaac978744be82f731cd8d
mheskett <mbheskett@gmail.com>
2018-10-08 17:57:18 -0700

    comments

--- simulate_link.py
+++ simulate_link.py
@@ -1,6 +1,7 @@
 """
 with overlapping windows of genome of variable size
 calculate L1 density, SNP density, GC content
-create a tool that 
+
+use pybedtools to generate windows, use big pandas data frame to index GTF files or bed files of genomic fatures
 
 """
