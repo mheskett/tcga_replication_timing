68c56c137e0336ea46e2bcc37ebb3746d620b5c2
mheskett <mbheskett@gmail.com>
2018-11-08 15:58:31 -0800

    FIXED BUG RECURSION

--- dev_tcga.py
+++ dev_tcga.py
@@ -89,110 +89,13 @@ def faster_simulate_links(length,window_fraction=0.25,snps=False,l1=False,gc=Fal
 
 	if len(window_snp_l1_nuc) < minimum:
 		wiggle+=0.05
-		results = faster_simulate_links(length=length,snps=snps,gc=gc,l1=l1,wiggle=wiggle,minimum=minimum) # recursive biatch
-		print(wiggle, " wiggle")
+		window_snp_l1_nuc = faster_simulate_links(length=length,snps=snps,gc=gc,l1=l1,wiggle=wiggle,minimum=minimum) # recursive biatch
+		print(wiggle, " wiggle") # not returning this!!!!
 	if len(window_snp_l1_nuc) > maximum:
 		window_snp_l1_nuc = window_snp_l1_nuc.head(n=100) # min is minimum, max is here.
-
-
 	
 	return window_snp_l1_nuc.loc[:,["chrom","start","end",
 					"snps/kb","pct_gc","fraction_l1"]]
-
-
-def simulate_links(length,window_fraction=0.25):
-
-	"""	
-	#/Users/heskett/tcga_replication_timing/data
-	returns some number of genomic windows of length=length
-	and statistically SIMILAR gc ,l1,snp
-	"""
-
-	start=time.time()
-	# snp doesnt need to be bedtool object yet i guess
-	snp = "/Users/mike/replication_tcga/data/snp150.ucsc.hg38.nochr.sorted.bed"
-	l1 = pybedtools.BedTool("/Users/mike/replication_tcga/data/L1_ucsc_hg38_nochr.bed")
-
-	# initiate bed tool and make windows
-	# all file must be sorted sort -k1,1 -k2,2n
-	a=pybedtools.BedTool()
-	windows=a.window_maker(g="/Users/mike/replication_tcga/data/hg38.nochr.sorted.fa.fai",
-							w=length,s=length*window_fraction)
-	wint=time.time()
-
-	print(str(wint-start)+" time for windows")
-
-	windows_nuc = windows.nucleotide_content(fi="/Users/mike/replication_tcga/data/hg38.nochr.fa")\
-							.to_dataframe() ## 0th column has colnames. current colname is garbage
-	gc=time.time()
-	print(str(gc-wint)+" time for gc")
-
-	## now widows needs to go into below procedure
-	windows_nuc.columns = windows_nuc.iloc[0]
-	windows_nuc = windows_nuc.drop(0,axis="rows")
-	keepers = [x for x in windows_nuc.columns if "user" in x or "pct_gc" in x]
-	windows_nuc = windows_nuc.loc[:,keepers]
-
-
-	## get snp density
-	windows_nuc = pybedtools.BedTool.from_dataframe(windows_nuc)
-	windows_nuc_snp = windows_nuc.intersect(snp,c=True,sorted=True)
-	snpt=time.time()
-	print(str(snpt-gc)+ " time for snp ") ## pre sorted file much faster sort -k1,1 -k2,2n
-	
-	## get L1
-	windows_nuc_snp_l1 = windows_nuc_snp.coverage(l1)
-	l1t=time.time()
-	print(str(l1t-snpt)+" time for l1 coverage")
-	
-	## manipulate data frame
-	df = windows_nuc_snp_l1.to_dataframe(names=["chrom","start","end",
-										"pct_gc","num_snps","cov1",
-										"cov2","cov3","fraction_l1"])
-	# create snps per kb column
-	df["snps/kb"] = df["num_snps"] / (length/1000)
-
-	# return columns of interest
-	final = df.loc[:,["chrom","start","end",
-					"snps/kb","pct_gc","fraction_l1"]]
-	
-	return final
-
-def get_similar_links(df,snps=False,l1=False,gc=False,wiggle=0.05,minimum=30):
-
-	"""
-	takes in DF of fake links of size of real link, and snp,l1,gc of REAL link
-	works with output of simulate links
-	does boolean selection of simulate links data frame
-	build distribution of snps/kb,pct_gc,fracl1, then select SIMILAR windows and return them
-	"""
-
-	# get percentile score of input parameters in fake link data frame
-	snps_score,gc_score,l1_score = (scipy.stats.percentileofscore(df["snps/kb"], score=snps, kind='rank'),
-									scipy.stats.percentileofscore(df["pct_gc"], score=gc, kind='rank'),
-									scipy.stats.percentileofscore(df["fraction_l1"], score=l1, kind='rank'))
-
-	results = df[
-		(df["snps/kb"]\
-			.between(left=df["snps/kb"].quantile(q=min_max(snps_score/100-wiggle)),
-					right=df["snps/kb"].quantile(q=min_max(snps_score/100+wiggle))))\
-		& (df["pct_gc"]\
-			.between(left=df["pct_gc"].quantile(q=min_max(gc_score/100-wiggle)),
-					right=df["pct_gc"].quantile(q=min_max(gc_score/100+wiggle))))\
-		& (df["fraction_l1"]\
-			.between(left=df["fraction_l1"].quantile(q=min_max(l1_score/100-wiggle)), # this is going below zero i think....
-					right=df["fraction_l1"].quantile(q=min_max(l1_score/100+wiggle))))]
-
-	if len(results) < minimum:
-		wiggle+=0.05
-		results = get_similar_links(df,snps=snps,gc=gc,l1=l1,wiggle=wiggle,minimum=minimum) # recursive biatch
-	print("wiggle: ",wiggle)
-
-	
-	# now check to see total number, if its too small, raise the quantile and go again
-	# can use bedtools merge -d to merge overlapping features with minimum distance
-
-	return results
 
 def unique_name(x):
 	# given a line of file, create unique name for link
